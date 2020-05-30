c568db4392f300a7db47c45dabd86d10182438c2
Reginald Tjitradi <rtjitradi@gmail.com>
2020-05-29 21:41:11 -0700

    passed all string1

--- string1.py
+++ string1.py
@@ -4,7 +4,7 @@ Kenzie assignment: String1
 """
 # Your name, plus anyone who helped you with this assignment.
 # Give credit where credit is due.
-__author__ = "???"
+__author__ = "Reggy Tjitradi and guided by Daniel's demo too"
 
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
@@ -28,8 +28,10 @@ __author__ = "???"
 
 
 def donuts(count):
-    # your code here
-    return
+    if count >= 10:
+        return "Number of donuts: many"
+    else:
+        return f"Number of donuts: {count}" 
 
 
 # B. both_ends
@@ -42,8 +44,10 @@ def donuts(count):
 
 
 def both_ends(s):
-    # your code here
-    return
+    if len(s) < 2:
+        return ""
+    else:
+        return s[:2] + s[-2:]
 
 
 # C. fix_start
@@ -58,8 +62,7 @@ def both_ends(s):
 
 
 def fix_start(s):
-    # your code here
-    return
+    return s[0] + s[1:].replace(s[0], "*")
 
 
 # D. mix_up
@@ -73,8 +76,7 @@ def fix_start(s):
 
 
 def mix_up(a, b):
-    # your code here
-    return
+    return a.replace(a[:2], b[:2]) + " " + b.replace(b[:2], a[:2])
 
 
 # Provided simple test() function used in main() to print
